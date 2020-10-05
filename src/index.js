d6598f02563f80410acd1b28da19017f2d4475a9
Mikhail Akhmedzyanov <mikhail.akhmedzyanov@gmail.com>
2020-10-05 20:33:41 +0300

    feat: done reverse-int task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
-  
+  let numberArr = n.toString().split('');
+  let char = numberArr[0];
+ 
+  if (char === '-') {
+    let resultNumber = numberArr.slice(1).reverse().join('')
+    return (Number(resultNumber));
+  }
+  else {
+    let resultNumber = numberArr.reverse().join('')
+    return (Number(resultNumber));
+  }
 }
